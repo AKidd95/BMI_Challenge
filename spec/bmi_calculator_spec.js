f2c9aef26411131b0e35d3fa5ba6f60d821ebf93
Isabelle <isabellelidberg@live.se>
2018-01-31 14:27:18 +0100

    added calcutater specs to imperial method

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -4,18 +4,19 @@ describe("BMICalculator", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+    person_metric = new Person({weight: 90, height: 186});
+    person_imperial = new Person({weight: 144, height: 69});
     calculator = new BMICalculator();
   });
 
   it("calculates BMI for a person using metric method", function() {
-    calculator.metric_bmi(person);
-    expect(person.bmiValue).toEqual(26.01);
+    calculator.metric_bmi(person_metric);
+    expect(person_metric.bmiValue).toEqual(26.01);
   });
 
   it("calculates BMI for a person using imperial method", function() {
-    calculator.imperial_bmi(person);
-    expect(person.bmiValue).toEqual(21.26);
+    calculator.imperial_bmi(person_imperial);
+    expect(person_imperial.bmiValue).toEqual(21.26);
   });
 
 
--- spec/person_spec.js
+++ spec/person_spec.js
@@ -2,24 +2,24 @@ describe("Person", function() {
   var person;
 
   beforeEach(function() {
-    person = new Person({weight: 90, height: 186});
+    person_metric = new Person({weight: 90, height: 186});
   });
 
   it("should have weight of 90", function() {
-    expect(person.weight).toEqual(90);
+    expect(person_metric.weight).toEqual(90);
   });
 
   it("should have height of 186", function() {
-    expect(person.height).toEqual(186);
+    expect(person_metric.height).toEqual(186);
   });
 
   it("should calculate BMI value", function() {
-    person.calculate_bmi();
-    expect(person.bmiValue).toEqual(26.01)
+    person_metric.calculate_bmi();
+    expect(person_metric.bmiValue).toEqual(26.01)
   });
 
   it("should have a BMI Message", function() {
-    person.calculate_bmi();
-    expect(person.bmiMessage).toEqual("Overweight")
+    person_metric.calculate_bmi();
+    expect(person_metric.bmiMessage).toEqual("Overweight")
   });
 });
